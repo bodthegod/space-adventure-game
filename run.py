f479d2f9cdfac3a392430a70fef9edfec9ece024
bodthegod <bodthegod@gmail.com>
2022-08-05 15:59:38 +0000

    Comment out start_game function to test if function works correctly, reorder code to print in correct order for user

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import sys
 import random
 
 
@@ -18,12 +20,14 @@ def start_game_select():
     """
     start_game_choice = input("Would you like to launch captain? yes/no \n")
     if start_game_choice == "yes":
-        start_game()
+        print("*spooling* Whirring up engines *humming*")
+        # start_game()
     elif start_game_choice == "no":
+        print("Shutting down systems")
         exit_game()
-        #break
     else:
         print("To start or exit the game, enter the text 'yes' or 'no'.")
+        start_game_select()
 
 
 def choose_char_name():
@@ -56,20 +60,29 @@ def weapon_select():
     print("Your weapon shifts and clicks into gear, whirring ready to fire.")
     print(f"The {class_choice} speaks to you, levitating towards your fingertips")
 
-#def start_game():
+# def start_game():
+#     """
+#     Starts game
+#     """
+#     main()
 
-#def exit_game():
+
+def exit_game():
+    """
+    Exits game
+    """
+    sys.exit()
 
 def main():
     """
     Main function to call all functions
     """
+    
     game_prologue()
     start_game_select()
     choose_char_name()
     char_class_info()
     weapon_select()
-    # start_game()
-    # exit_game()
+    
 
 main()
