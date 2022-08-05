82e5f044bf7989c1ef62cde4be053616bdc8b5f5
bodthegod <bodthegod@gmail.com>
2022-08-05 14:47:21 +0000

    Define choose_char_name function to set the player name

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def choose_char_name():
+    """
+    User selects space name
+    """
+    char_name = input("Choose your alien name:\n")
+    print(f"Greetings, {char_name}")
+
+
+choose_char_name()
