0a0493d132c3232fe65606305b04311f4b7f9fba
bodthegod <bodthegod@gmail.com>
2022-08-05 15:32:51 +0000

    Define char_class_info function to introduce the player to lore and create random_class_choice variable

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ def start_game_select():
         start_game()
     elif start_game_choice == "no":
         exit_game()
+        #break
     else:
         print("To start or exit the game, enter the text 'yes' or 'no'.")
 
@@ -23,16 +24,26 @@ def choose_char_name():
     char_name = input("Choose your alien name:\n")
     print(f"Greetings, {char_name}")
 
-
+def char_class_info():
+    """
+    Prints class info and start to game lore
+    """
+    print('"Hey, you!"')
+    print('"In order to take over the galaxy, you may need one of these!"')
+    print('"Walk over there to your arsenal and allow it to choose you."')
+    
 def weapon_select():
     """
-    Gives user a class for their character
+    Gives user a class for their character (random)
     """
-    class_choice = random.choice([
+    class_choice = [
         "Scavenger Laser",
         "Strange Glowing Pistol",
         "Rusty Railgun",
-        "Uranium Repeater"])
+        "Uranium Repeater"]
+
+    random_class_choice = random.choice(class_choice)
+
     print("Your weapon shifts and clicks into gear, whirring ready to fire.")
     print(f"The {class_choice} speaks to you, levitating towards your fingertips")
 
@@ -44,8 +55,9 @@ def main():
     """
     Main function to call all functions
     """
-    choose_char_name()
     start_game_select()
+    choose_char_name()
+    char_class_info()
     weapon_select()
     # start_game()
     # exit_game()
