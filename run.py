415aab0e73b9400b3c34264dc9d7620840703d18
bodthegod <bodthegod@gmail.com>
2022-08-05 15:17:26 +0000

    Add weapon select with random choice for player, import random to create random choice within weapon selection and define start and exit game functions

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
 
 def start_game_select():
     """
@@ -13,6 +15,7 @@ def start_game_select():
     else:
         print("To start or exit the game, enter the text 'yes' or 'no'.")
 
+
 def choose_char_name():
     """
     User selects space name
@@ -21,11 +24,30 @@ def choose_char_name():
     print(f"Greetings, {char_name}")
 
 
+def weapon_select():
+    """
+    Gives user a class for their character
+    """
+    class_choice = random.choice([
+        "Scavenger Laser",
+        "Strange Glowing Pistol",
+        "Rusty Railgun",
+        "Uranium Repeater"])
+    print("Your weapon shifts and clicks into gear, whirring ready to fire.")
+    print(f"The {class_choice} speaks to you, levitating towards your fingertips")
+
+#def start_game():
+
+#def exit_game():
+
 def main():
     """
     Main function to call all functions
     """
     choose_char_name()
     start_game_select()
+    weapon_select()
+    # start_game()
+    # exit_game()
 
 main()
