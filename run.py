cf27bd348c090eec1ebe1ba6d714ec17b85451ba
bodthegod <bodthegod@gmail.com>
2022-08-05 15:00:29 +0000

    Define start_game_select function to give user choice of starting or quitting the game, and define main function to call main functions

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
-# Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def start_game_select():
+    """
+    Gives user options to start or end game
+    """
+    start_game_choice = input("Would you like to launch captain? yes/no \n")
+    if start_game_choice == "yes":
+        start_game()
+    elif start_game_choice == "no":
+        exit_game()
+    else:
+        print("To start or exit the game, enter the text 'yes' or 'no'.")
 
 def choose_char_name():
     """
@@ -10,4 +21,11 @@ def choose_char_name():
     print(f"Greetings, {char_name}")
 
 
-choose_char_name()
+def main():
+    """
+    Main function to call all functions
+    """
+    choose_char_name()
+    start_game_select()
+
+main()
