6e58cd878b2aa87f5f62801d80662f0db256fb07
bodthegod <bodthegod@gmail.com>
2022-08-08 14:48:35 +0000

    Create UserNumbers class to refactor run.py code for readability and ease of use

--- choices.py
+++ choices.py
@@ -33,3 +33,52 @@ class UserChoices:
         Function runs if user selects choice other than yes or no
         """
         return f"\n{self.choice_else} \n"
+
+class UserNumbers:
+    """
+    Number selection class for storyline advancements
+    """
+    def __init__(self, number_one, number_two, number_three):
+        """
+        Instancing 1, 2 and 3 number selection 
+        """
+        self.number_one = number_one
+        self.number_two = number_two
+        self.number_three = number_three
+
+
+    def number_one(self):
+        """
+        Function runs if user selects 1
+        """
+        return f"\n{self.number_one} \n"
+
+
+    def number_two(self):
+        """
+        Function runs if user selects 2
+        """
+        return f"\n{self.number_two} \n"
+
+
+    def number_three(self):
+        """
+        Function runs if user selects 3
+        """
+        return f"\n{self.number_three} \n"
+
+class UserNumbersElse:
+    """
+    Class for else statement in UserNumbers selection
+    """
+    def __init__(self):
+        """
+        Instancing else option if user selects other than 1, 2 or 3
+        """
+        self.number_else = "Enter 1, 2 or 3 to choose a route."
+
+    def user_number_else(self):
+        """
+        Function runs when user selects other than 1, 2 or 3
+        """
+        return f"\n{self.number_else}"
